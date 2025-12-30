204d5303a27e74b21ec6333430baccae532df771
Sebastian <seb.kontakt.sob@gmail.com>
2025-12-30 12:37:43 +0100

    Nothing of note

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 <link rel="stylesheet" href="assets/css/sidebar.css">
-<link rel="stylesheet" href="assets/css/index.css">
 
 <title>Sigma Notes</title>
 
